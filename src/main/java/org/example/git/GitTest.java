f6703c1dbcc6ce1cc2495072af587c7b81643398
peter <xuduo_2000@163.com>
2023-08-02 09:31:05 +0800

    second commit

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -12,5 +12,6 @@ package org.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
