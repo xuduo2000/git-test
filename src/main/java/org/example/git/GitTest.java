315e6a087fa9a252dcf574cd41d9f494134ebae5
peter <xuduo_2000@163.com>
2023-08-02 09:29:02 +0800

    first commit

---
+++ src/main/java/org/example/git/GitTest.java
@@ -0,0 +1,16 @@
+package org.example.git;
+
+/**
+ * ClassName: GitTest
+ * Package: org.example.git
+ * Description:
+ *
+ * @Author Peter
+ * @Create 2023/8/2 9:23
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
